6e280fad67af79a21edd2a4c7dc299b18c2ee5a5
Ian Channing <ian@klever.co.uk>
2015-08-17 01:22:26 +0200

    Bug fix, get the project index image displaying
    1. In bootstrap.php replace the dirname(__FILE__) with __DIR__ as we're assuming PHP 5.3 minimum
    1. In Model/AppModel.php and NodeImages.php move the method getPathUrl to convert a nemxe path into a URL as it's going to be used by projects as well as Nodes
    2. In Models/Projects.php replace all direct access to path property with calls to getPath, in getTitleImage method convert the path to the image with a URL

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,8 @@
 
 use Config\Config;
 
+define('DS', DIRECTORY_SEPARATOR);
+
 /**
  * Autoload the app classes
  *
@@ -11,24 +13,22 @@ use Config\Config;
  *
  * @link http://php.net/manual/en/function.spl-autoload.php#92767
  */
-set_include_path(get_include_path() . PATH_SEPARATOR . 'app/');
+set_include_path(get_include_path() . PATH_SEPARATOR . 'app'.DS);
 spl_autoload_register();
-
-define('DS', DIRECTORY_SEPARATOR);
 
 // Config\Config isn't recognised as a class until after the autoloading
 date_default_timezone_set(Config::TIMEZONE);
 
-require_once dirname(__FILE__) . DS.'vendor'.DS.'autoload.php';
+require_once __DIR__ . DS.'vendor'.DS.'autoload.php';
 
 /**
  * @todo This shouldn't just be a hard coded require
  */
-require_once dirname(__FILE__) . DS.'vendor'.DS.'ianchanning'.DS.'vandaphp-core'.DS.'src'.DS.'bootstrap.php';
+require_once __DIR__ . DS.'vendor'.DS.'ianchanning'.DS.'vandaphp-core'.DS.'src'.DS.'bootstrap.php';
 
 require_once 'utils_vanda.php';
 
 /**
  * Nemex index.php code
  */
-define('NX_PATH', dirname(__FILE__).DS);
+define('NX_PATH', __DIR__.DS);
