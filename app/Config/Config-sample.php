218b0bcfd13d6ba59047d75ca5e51da5499303b0
Ian Channing <ian@klever.co.uk>
2015-08-16 05:40:11 +0200

    Create an AppView for overriding the Vanda View methods, allow the markdown css to be specified in the config
    1. In bootstrap.php specify the directory path using DIRECTORY_SEPARATOR constants
    2. In the Config/Config-sample.php add in a MARKDOWN_CSS constant to allow switching markdown styles
    3. In Controllers/AppController.php, in __construct, instantiate AppView as this->view, remove setting the config paramter - allow it to be called statically in the views themselves
    4. New Views/AppView.php, to allow overriding the Vanda View methods
    5. In layouts/default.head.php - just whitespace change
    6. In projects/readonly.php and view.php specify the full namespace to the static Config class as we're no longer passing it through as a view variable

--- app/Config/Config-sample.php
+++ app/Config/Config-sample.php
@@ -11,14 +11,27 @@ class Config {
 
 	const PROJECTS_PATH = 'projects'.DIRECTORY_SEPARATOR;
 
-	// Maximum width for Images. Beyond this, a scaled down version is
-	// created
+	/**
+	 * Maximum width for Images. Beyond this, a scaled down version is
+	 * created
+	 */
 	const IMAGE_BIG_PATH = 'big'.DIRECTORY_SEPARATOR;
 	const IMAGE_MAX_WIDTH = 800;
 	const IMAGE_JPEG_QUALITY = 95;
 	const IMAGE_SHARPEN = true;
 
-	// The TIMEZONE setting is only used if there's no explicit
-	// timezone set in the php.ini
+	/**
+	 * The TIMEZONE setting is only used if there's no explicit
+	 * timezone set in the php.ini
+	 */
 	const TIMEZONE = 'UTC';
+
+	/**
+	 * Custom markdown css styles
+	 * put a CSS file e.g example-markdown.css into app/webroot/assets/css/
+	 * set this option to 'example'
+	 * installed options are github | simonlc
+	 * to use none set it to ''
+	 */
+	const MARKDOWN_CSS = 'github';
 }
--- app/Views/layouts/default.head.php
+++ app/Views/layouts/default.head.php
@@ -8,7 +8,11 @@
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 	<link rel="stylesheet" type="text/css" href="app/webroot/assets/css/screen.css">
-	<link rel="stylesheet" type="text/css" href="app/webroot/assets/css/github-markdown.css">
+	<?php if (!empty(Config\Config::MARKDOWN_CSS)) { ?>
+
+	<link rel="stylesheet" type="text/css" href="app/webroot/assets/css/<?php echo Config\Config::MARKDOWN_CSS; ?>-markdown.css">
+	<?php } ?>
+
 	<link rel="stylesheet" type="text/css" href="app/webroot/assets/css/small-screen-overrides.css" media="screen and (max-width: 759px)">
 	<link rel="apple-touch-icon" sizes="76x76" href="app/webroot/assets/img/touch-icon-ipad.png">
 	<link rel="apple-touch-icon" sizes="120x120" href="app/webroot/assets/img/touch-icon-iphone-retina.png">
